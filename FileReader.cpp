c752045df7e3671b8223f631b7c20bff47a55217
p1ma <pierre.junges@orange.fr>
2016-08-12 10:21:26 +0200

    readFile

--- FileReader.cpp
+++ FileReader.cpp
@@ -1,7 +1,52 @@
 #include "FileReader.hpp"
 
+// constructor
 FileReader::FileReader(){
+  this->pFile = new std::ifstream();
+  this->directory = "/patterns/images/";
 }
 
+// destructor
 FileReader::~FileReader(){
+  delete this->pFile; // delete file pointer
 }
+
+// check name's properties
+std::string FileReader::readFile(const std::string& name){
+  std::string suffix;
+  std::string png = "png";
+  int size = name.size();
+  if(size != 0){
+    if(name.compare((size - 3), 3,png) == 0){
+      std::string file = directory;
+      file += name.c_str();
+      if(exist(file)){
+	return FileReader::read(file);
+      }else{
+	return "(readFile) FILE DOES NOT EXIST";
+      }
+    }else{
+      return "(readFile) FILE .PNG ONLY";
+    }
+  }else{
+    return "(readFile) INVALID NAME";
+  }
+}
+
+// check file 'name' existence
+inline bool FileReader::exist(const std::string& name){
+  FILE *exist;
+  if((exist = fopen(name.c_str(), "r"))){
+    fclose(exist);
+    delete exist;
+    return true;
+  }
+  return false;
+}
+
+// read
+std::string FileReader::read(const std::string& name){
+  return "OK";
+}
+
+
--- FileReader.hpp
+++ FileReader.hpp
@@ -1,14 +1,21 @@
-#ifndef FILEREADER
-#define FILEREADER
+#ifndef FILE_READER
+#define FILE_READER
+
+#include <iostream>
+#include <fstream>
 
 class FileReader {
 
 private :
-
+  std::ifstream *pFile;
+  std::string directory;
+  inline bool exist(const std::string& name);
+  std::string read(const std::string& name);
 public :
   FileReader(); // constructor
   ~FileReader(); // destructor
+  std::string readFile(const std::string& name); // check existence / pName's size before calling method read()
 
 };
 
-#endif // FILEREADER
+#endif // FILE_READER
